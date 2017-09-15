79a1dddf3b786b1e1aad8f4369255611c51fcb4a
Matt <young.qubit@gmail.com>
2017-09-15 00:51:36 -0700

    Finished ch1

--- window.cpp
+++ window.cpp
@@ -1,19 +1,24 @@
+#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
-#define WINDOW_TITLE_PREFIX "YES"
+#define WINDOW_TITLE_PREFIX "YES:"
 
 int
   CurrentWidth = 800,
   CurrentHeight = 600,
   WindowHandle = 0;
 
+unsigned FrameCount = 0;
+
 void Initialize(int, char*[]);
 void InitWindow(int, char*[]);
 void ResizeFunction(int, int);
 void RenderFunction(void);
+void TimerFunction(int);
+void IdleFunction(void);
 
 int main(int argc, char* argv[])
 {
@@ -26,7 +31,20 @@ int main(int argc, char* argv[])
 
 void Initialize(int argc, char* argv[])
 {
+  GLenum GlewInitResult;
+
   InitWindow(argc, argv);
+
+  GlewInitResult = glewInit();
+
+  if (GLEW_OK != GlewInitResult) {
+    fprintf(
+        stderr,
+        "[ERROR]: %s\n",
+        glewGetErrorString(GlewInitResult)
+    );
+    exit(EXIT_FAILURE);
+  }
   
   fprintf(
     stdout,
@@ -34,7 +52,7 @@ void Initialize(int argc, char* argv[])
     glGetString(GL_VERSION)
   );
 
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClearColor(0.3f, 1.0f, 0.0f, 0.0f);
 }
 
 void InitWindow(int argc, char* argv[])
@@ -66,6 +84,8 @@ void InitWindow(int argc, char* argv[])
 
   glutReshapeFunc(ResizeFunction);
   glutDisplayFunc(RenderFunction);
+  glutIdleFunc(IdleFunction);
+  glutTimerFunc(0, TimerFunction, 0);
 }
 
 void ResizeFunction(int Width, int Height)
@@ -77,8 +97,37 @@ void ResizeFunction(int Width, int Height)
 
 void RenderFunction(void)
 {
+  ++FrameCount;
+
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glutSwapBuffers();
   glutPostRedisplay();
+}
+
+void IdleFunction(void)
+{
+  glutPostRedisplay();
+}
+
+void TimerFunction(int Value)
+{
+  if (0 != Value) {
+    char* TempString = (char*)
+      malloc(512 + strlen(WINDOW_TITLE_PREFIX));
+
+    sprintf(
+        TempString,
+        "%s: %d FPS @ %d x %d",
+        WINDOW_TITLE_PREFIX,
+        FrameCount * 4,
+        CurrentWidth,
+        CurrentHeight
+    );
+    glutSetWindowTitle(TempString);
+    free(TempString);
+  }
+
+  FrameCount = 0;
+  glutTimerFunc(250, TimerFunction, 1);
 }
